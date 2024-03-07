243a3bf50382ed8b7e37955835dba08e6e0a49d8
Gabriel <92476790+github-gabriel@users.noreply.github.com>
2024-03-07 16:42:48 +0100

    New modes and prompts, RAW conversion, UI updates

--- photo-guru-client/app.js
+++ photo-guru-client/app.js
@@ -8,12 +8,22 @@ const answer = document.getElementById('answer')
 
 const darkModeToggle = document.getElementById('toggle')
 
+const checkboxContainer = document.querySelector('.checkbox-container')
+
 const darkIcon = 'icons/moon.svg'
 const lightIcon = 'icons/sun.svg'
 
+const editCheckbox = document.getElementById('edit')
+const similarCheckbox = document.getElementById('similar')
+const keepCheckbox = document.getElementById('keep')
+const ideaCheckbox = document.getElementById('idea')
+
 let selectedFiles = null
+let mode = null
 
 uploadButton.disabled = true
+
+editCheckbox.checked = true
 
 window.onload = function () {
   let theme = localStorage.getItem('theme')
@@ -24,11 +34,84 @@ window.onload = function () {
   }
 }
 
+editCheckbox.addEventListener('click', function () {
+  if (editCheckbox.checked) {
+    mode = 'image_comparison_editing'
+    fileInput.multiple = true
+  }
+})
+
+similarCheckbox.addEventListener('click', function () {
+  if (similarCheckbox.checked) {
+    mode = 'image_comparison_subject'
+    fileInput.multiple = true
+  }
+})
+
+keepCheckbox.addEventListener('click', function () {
+  if (keepCheckbox.checked) {
+    mode = 'keep_or_delete'
+    fileInput.multiple = false
+  }
+})
+
+ideaCheckbox.addEventListener('click', function () {
+  if (ideaCheckbox.checked) {
+    mode = 'idea'
+    fileInput.multiple = false
+  }
+})
+
 darkModeToggle.addEventListener('click', function () {
   if (document.body.classList.contains('dark-theme')) {
     setTheme('light')
   } else {
     setTheme('dark')
+  }
+})
+
+checkboxContainer.addEventListener('click', function (event) {
+  if (event.target.type === 'checkbox') {
+    const checkboxes = checkboxContainer.querySelectorAll('input[type="checkbox"]')
+    for (let i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i] !== event.target) {
+        checkboxes[i].checked = false // Uncheck other checkboxes
+      }
+    }
+  }
+})
+
+dropzone.addEventListener('dragover', (event) => {
+  event.preventDefault()
+  dropzone.classList.add('hover')
+})
+
+dropzone.addEventListener('dragleave', () => {
+  dropzone.classList.remove('hover')
+})
+
+dropzone.addEventListener('drop', dropHandler) // Call defined function
+
+// Handle click on the input field
+fileInput.addEventListener('change', (event) => {
+  event.preventDefault()
+  uploadButton.disabled = false
+  selectedFiles = event.target.files
+
+  showPreviews(selectedFiles)
+})
+
+uploadButton.addEventListener('click', () => {
+  if (selectedFiles && selectedFiles.length > 1 && fileInput.multiple === true || selectedFiles && selectedFiles.length === 1 && fileInput.multiple === false) {
+    uploadFile(selectedFiles)
+    deletePreview()
+    selectedFiles = null // Clear selected files after upload
+  } else if (!selectedFiles) {
+    alert('Please select files to upload.')
+  } else if (selectedFiles && selectedFiles.length > 1 && fileInput.multiple === false) {
+    alert('Please select only one file.')
+  } else if (selectedFiles && selectedFiles.length === 1 && fileInput.multiple === true) {
+    alert('Please select multiple files.')
   }
 })
 
@@ -57,35 +140,18 @@ function dropHandler (event) {
   showPreviews(selectedFiles)
 }
 
-dropzone.addEventListener('dragover', (event) => {
-  event.preventDefault()
-  dropzone.classList.add('hover')
-})
+function isRawFileExtension (fileName) {
+  // Split the filename by the dot (.) to separate extension
+  const parts = fileName.split('.')
+  // Get the extension in lowercase for case-insensitive comparison
+  const extension = parts[parts.length - 1].toLowerCase()
 
-dropzone.addEventListener('dragleave', () => {
-  dropzone.classList.remove('hover')
-})
+  // Array of common raw image extensions
+  const rawExtensions = ['rw2']
 
-dropzone.addEventListener('drop', dropHandler) // Call defined function
-
-// Handle click on the input field
-fileInput.addEventListener('change', (event) => {
-  event.preventDefault()
-  uploadButton.disabled = false
-  selectedFiles = event.target.files
-
-  showPreviews(selectedFiles)
-})
-
-uploadButton.addEventListener('click', () => {
-  if (selectedFiles) {
-    uploadFile(selectedFiles)
-    deletePreview()
-    selectedFiles = null // Clear selected files after upload
-  } else {
-    alert('Please select files to upload.')
-  }
-})
+  // Check if the extension is present in the array
+  return rawExtensions.includes(extension)
+}
 
 function deletePreview () {
   const previewGrid = document.querySelector('.preview-grid')
@@ -103,9 +169,15 @@ function showPreviews (files) {
     preview.classList.add('preview')
 
     // Image preview
-    const img = document.createElement('img')
-    img.src = URL.createObjectURL(file)
-    preview.appendChild(img)
+    if (isRawFileExtension(file.name)) {
+      const p = document.createElement('p')
+      p.textContent = file.name
+      preview.appendChild(p)
+    } else {
+      const img = document.createElement('img')
+      img.src = URL.createObjectURL(file)
+      preview.appendChild(img)
+    }
 
     // Close button to remove the image
     const close = document.createElement('img')
@@ -132,6 +204,7 @@ function showPreviews (files) {
 }
 
 function uploadFile (files) {
+  answer.textContent = '' // Clear answer
   uploadButton.disabled = true // Disable the button
 
   // Loading animation
@@ -145,6 +218,13 @@ function uploadFile (files) {
   }
   if (hintInput.value) {
     formData.append('hint', hintInput.value)
+  }
+  if (mode) {
+    formData.append('mode', mode)
+  } else {
+    alert('Please select a mode')
+    loadingContainer.style.visibility = 'hidden' // Hide loading animation
+    return
   }
 
   // Use Axios for the request
--- photo-guru-server/main.py
+++ photo-guru-server/main.py
@@ -5,12 +5,31 @@ from openai import OpenAI
 import os
 import base64
 import logging
-import time
+from rawpy import imread
+from PIL import Image
 
 client = OpenAI()
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173"])  # Allow requests from the frontend
+
+GENERAL_PROMPT = "Answer in a few sentences in English recommending a choice or stating a clear decision without using Markdown."
+
+RAW_TO_JPG_QUALITY = 65
+
+PROMPTS = {
+    "image_comparison_editing": "What’s the editing differences made between these images and which one would you choose because of those editing choices?" + GENERAL_PROMPT,
+    "keep_or_delete": "If you theoretically had to decide between keeping or deleting this image, which one would you choose and why?" + GENERAL_PROMPT,
+    "image_comparison_subject": "These images depict the same subject but from different angles or changes made to the subject. Which one would you choose and why?" + GENERAL_PROMPT,
+    "idea": "What would your goal for an edit of this photo be and what would you do/ how would you edit the photo to achieve that goal?" + GENERAL_PROMPT
+}
+
+HINT_PROMPTS = {
+    "image_comparison_editing": "A hint concerning the editing changes made between the images to help you find the difference",
+    "keep_or_delete": "A hint concerning factors that would help you decide whether to keep or delete the image",
+    "image_comparison_subject": "A hint concerning the differences between the images to help you decide which one to choose",
+    "idea": "A hint concerning the goal for an edit of this photo to help you decide how to edit the photo to achieve that goal"
+}
 
 UPLOAD_FOLDER = 'uploads'  # Folder to store uploaded images
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -27,7 +46,7 @@ console_handler.setLevel(logging.DEBUG)  # Set handler level to DEBUG
 logger.addHandler(console_handler)
 
 
-def construct_image_payload(images):
+def construct_image_payload(images, detail="high"):
     final_payload = []
     for image in images:
         logger.debug(f"Constructing image payload for image: {image[:20]}...")
@@ -35,22 +54,32 @@ def construct_image_payload(images):
             "type": "image_url",
             "image_url": {
                 "url": f"data:image/jpeg;base64,{image}",
-                "detail": "high"
+                "detail": detail
             }
         }
         final_payload.append(payload)
     return final_payload
 
 
-def construct_hint_payload(hint):
+def construct_hint_payload(hint, mode):
     logger.debug(f"Constructing hint payload for hint: {hint}")
     return {
         "type": "text",
-        "text": f"A hint concerning the editing changes made between the images to help you find the difference: {hint}"
+        "text": f"{HINT_PROMPTS[mode]}: {hint}"
     }
 
 
-def compare_images(files, hint):
+def raw_to_jpg(raw_file, jpg_file):
+    print(raw_file)
+    print(jpg_file)
+    with imread(raw_file) as raw:
+        rgb_image = raw.postprocess()
+
+    pil_image = Image.fromarray(rgb_image)
+    pil_image.save(jpg_file, quality=RAW_TO_JPG_QUALITY)
+
+
+def compare_images(files, hint, mode):
     base64_images = []
     for file in files:
 
@@ -63,8 +92,25 @@ def compare_images(files, hint):
 
             base64_image = encode_image(filepath)
             base64_images.append(base64_image)
+
+            os.remove(filepath)
+        elif file and allowed_file(file.filename, {'rw2'}):
+            filename = secure_filename(file.filename)
+
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)  # Save the RAW file
+
+            jpg_file = os.path.join(app.config['UPLOAD_FOLDER'], f"{filename}.jpg")  # JPG file
+            raw_to_jpg(filepath, jpg_file)  # Save the RAW file as a JPG file
+
+            base64_image = encode_image(jpg_file)  # Encode the JPG file to base64
+            base64_images.append(base64_image)
+
+            os.remove(filepath)
+            os.remove(jpg_file)
         else:
             logger.error(f"File {file.filename} is not a valid image file or does not have a valid extension")
+            return
 
     images_payload = construct_image_payload(base64_images)
     response = client.chat.completions.create(
@@ -75,9 +121,9 @@ def compare_images(files, hint):
                 "content": [
                     {
                         "type": "text",
-                        "text": "What’s the editing differences made between these images and which one would you choose because of those editing choices?"
+                        "text": PROMPTS[mode]
                     },
-                    *([construct_hint_payload(hint)] if hint is not None else []),
+                    *([construct_hint_payload(hint, mode)] if hint is not None else []),
                     *images_payload
                 ],
             }
@@ -94,9 +140,11 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 
-def allowed_file(filename):
+def allowed_file(filename, allowed_extensions=None):
+    if allowed_extensions is None:
+        allowed_extensions = ALLOWED_EXTENSIONS
     return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
 
 @app.route('/upload', methods=['POST'])
@@ -105,13 +153,17 @@ def upload_images():
     if 'images' not in request.files:
         logger.error("The request does not contain any files")
         return jsonify({'error': 'No files uploaded'}), 400
-    if 'hint' in request.form:
+    if 'mode' not in request.form:
+        logger.error("Mode is missing from the request")
+        return jsonify({'error': 'No mode was provided'}), 400
+    if 'hint' in request.form:  # Optional hint
         logger.info(f"Found hint in request")
         hint = request.form['hint']
 
+    mode = request.form['mode']
     files = request.files.getlist('images')
 
-    return jsonify({'message': compare_images(files, hint)}), 201
+    return jsonify({'message': compare_images(files, hint, mode)}), 201
 
 
 if __name__ == "__main__":
